b4ab51d2227d3ac7a1cfc583b0ec045ed48e7b5d
dalba1992 <dalba1992@yandex.com>
2019-08-12 23:29:13 +0800

    www folder include

---
+++ www/listcopy-listcopy-module-es5.js
@@ -0,0 +1,130 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["listcopy-listcopy-module"],{
+
+/***/ "./node_modules/raw-loader/index.js!./src/app/listcopy/listcopy.page.html":
+/*!***********************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/listcopy/listcopy.page.html ***!
+  \***********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header>\r\n  <ion-toolbar>\r\n    <ion-title>\r\n      Hanuman\r\n    </ion-title>\r\n  </ion-toolbar>\r\n</ion-header>\r\n\r\n<ion-content>\r\n  <div margin>\r\n      <p>Sri Hanuman Chalisa</p>\r\n\r\n      <p>Doha</p>\r\n      \r\n      <p>Shri Guru Charan Sarooja-raj Nija manu Mukura Sudhaari\r\n      Baranau Rahubhara Bimala Yasha Jo Dayaka Phala Chari\r\n      Budhee-Heen Thanu Jannikay Sumirow Pavana Kumara\r\n      Bala-Budhee Vidya Dehoo Mohee Harahu Kalesha Vikaara</p>\r\n      \r\n      <p>Chopai</p>\r\n      \r\n      <p>Jai Hanuman gyan gun sagar\r\n      Jai Kapis tihun lok ujagar</p>\r\n      \r\n      <p>Ram doot atulit bal dhama\r\n      Anjaani-putra Pavan sut nama</p>\r\n      \r\n      <p>Mahabir Bikram Bajrangi\r\n      Kumati nivar sumati Ke sangi</p>\r\n      \r\n      <p>Kanchan varan viraj subesa\r\n      Kanan Kundal Kunchit Kesha</p>\r\n      \r\n      <p>Vajra Aur Dhuvaje Viraje\r\n      Kaandhe moonj janehu sajai</p> \r\n      \r\n      <p>suvan kesri Nandan\r\n      Tej prataap maha jag vandan</p> \r\n      \r\n      <p>guni ati chaturRam kaj karibe ko aatur</p> \r\n      \r\n      <p>Prabu charitra sunibe-ko rasiya\r\n      Ram Lakhan Sita man Basiya</p>\r\n      \r\n      <p>Sukshma roop dhari Siyahi dikhava\r\n      Vikat roop dhari lank jarava</p>\r\n      \r\n      <p>Bhima roop dhari asur sanghare\r\n      Ramachandra ke kaj sanvare</p>\r\n      \r\n      <p>Laye Sanjivan Lakhan Jiyaye\r\n      Shri Raghuvir Harashi ur laye</p>\r\n      \r\n      <p>Raghupati Kinhi bahut badai\r\n      Tum mam priye Bharat-hi-sam bhai</p>\r\n      \r\n      <p>Sahas badan tumharo yash gaave\r\n      Asa-kahi Shripati kanth lagaave</p>\r\n      \r\n      <p>Sankadhik Brahmaadi Muneesa\r\n      Narad-Sarad sahit Aheesa</p>\r\n      \r\n      <p>Yam Kuber Digpaal Jahan te\r\n      Kavi kovid kahi sake kahan te</p>\r\n      \r\n      <p>Tum upkar Sugreevahin keenha\r\n      Ram milaye rajpad deenha</p>\r\n      \r\n      <p>Tumharo mantra Vibheeshan maana\r\n      Lankeshwar Bhaye Sub jag jana</p>\r\n      \r\n      <p>Yug sahastra jojan par Bhanu\r\n      Leelyo tahi madhur phal janu</p>\r\n      \r\n      <p>Prabhu mudrika meli mukh mahee\r\n      Jaladhi langhi gaye achraj nahee</p>\r\n      \r\n      <p>Durgaam kaj jagath ke jete\r\n      Sugam anugraha tumhre tete</p>\r\n      \r\n      <p>Ram dwaare tum rakhvare\r\n      Hoat na agya binu paisare</p>\r\n      \r\n      <p>Sub sukh lahae tumhari sar na\r\n      Tum rakshak kahu ko dar naa</p>\r\n      \r\n      <p>Aapan tej samharo aapai\r\n      Teenhon lok hank te kanpai</p>\r\n      \r\n      <p>Bhoot pisaach Nikat nahin aavai\r\n      Mahavir jab naam sunavae</p>\r\n      \r\n      <p>Nase rog harae sab peera\r\n      Japat nirantar Hanumant beera</p>\r\n      \r\n      <p>Sankat se Hanuman chudavae\r\n      Man Karam Vachan dyan jo lavai</p>\r\n      \r\n      <p>Sab par Ram tapasvee raja\r\n      Tin ke kaj sakal Tum saja</p>\r\n      \r\n      <p>Aur manorath jo koi lavai\r\n      Sohi amit jeevan phal pavai</p>\r\n      \r\n      <p>Charon Yug partap tumhara\r\n      Hai persidh jagat ujiyara</p>\r\n      \r\n      <p>Sadhu Sant ke tum Rakhware\r\n      Asur nikandan Ram dulhare</p>\r\n      \r\n      <p>Ashta-sidhi nav nidhi ke dhata\r\n      As-var deen Janki mata</p>\r\n      \r\n      <p>Ram rasayan tumhare pasa\r\n      Sada raho Raghupati ke dasa</p>\r\n      \r\n      <p>Tumhare bhajan Ram ko pavai\r\n      Janam-janam ke dukh bisraavai</p>\r\n      \r\n      <p>Anth-kaal Raghuvir pur jayee\r\n      Jahan janam Hari-Bakht Kahayee</p>\r\n      \r\n      <p>Aur Devta Chit na dharehi\r\n      Hanumanth se hi sarve sukh karehi</p>\r\n      \r\n      <p>Sankat kate-mite sab peera\r\n      Jo sumirai Hanumat Balbeera</p>\r\n      \r\n      <p>Jai Jai Jai Hanuman Gosahin\r\n      Kripa Karahu Gurudev ki nyahin</p>\r\n      \r\n      <p>Jo sat bar path kare kohi\r\n      Chutehi bandhi maha sukh hohi</p>\r\n      \r\n      <p>Jo yah padhe Hanuman Chalisa\r\n      Hoye siddhi sakhi Gaureesa</p>\r\n      \r\n      <p>Tulsidas sada hari chera\r\n      Keejai Nath Hridaye mein dera</p>\r\n      \r\n      <p>Doha</p>\r\n      \r\n      <p>Pavan Tanay Sankat Harana\r\n      Mangala Murati Roop\r\n      Ram Lakhana Sita Sahita\r\n      Hriday Basahu Soor Bhoop</p>     \r\n  </div>\r\n</ion-content>\r\n\r\n"
+
+/***/ }),
+
+/***/ "./src/app/listcopy/listcopy.module.ts":
+/*!*********************************************!*\
+  !*** ./src/app/listcopy/listcopy.module.ts ***!
+  \*********************************************/
+/*! exports provided: ListCopyPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ListCopyPageModule", function() { return ListCopyPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _listcopy_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./listcopy.page */ "./src/app/listcopy/listcopy.page.ts");
+
+
+
+
+
+
+
+var ListCopyPageModule = /** @class */ (function () {
+    function ListCopyPageModule() {
+    }
+    ListCopyPageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["IonicModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_5__["RouterModule"].forChild([
+                    {
+                        path: '',
+                        component: _listcopy_page__WEBPACK_IMPORTED_MODULE_6__["ListCopyPage"]
+                    }
+                ])
+            ],
+            declarations: [_listcopy_page__WEBPACK_IMPORTED_MODULE_6__["ListCopyPage"]]
+        })
+    ], ListCopyPageModule);
+    return ListCopyPageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/listcopy/listcopy.page.scss":
+/*!*********************************************!*\
+  !*** ./src/app/listcopy/listcopy.page.scss ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2xpc3Rjb3B5L2xpc3Rjb3B5LnBhZ2Uuc2NzcyJ9 */"
+
+/***/ }),
+
+/***/ "./src/app/listcopy/listcopy.page.ts":
+/*!*******************************************!*\
+  !*** ./src/app/listcopy/listcopy.page.ts ***!
+  \*******************************************/
+/*! exports provided: ListCopyPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ListCopyPage", function() { return ListCopyPage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var ListCopyPage = /** @class */ (function () {
+    function ListCopyPage() {
+        this.icons = [
+            'flask',
+            'wifi',
+            'beer',
+            'football',
+            'basketball',
+            'paper-plane',
+            'american-football',
+            'boat',
+            'bluetooth',
+            'build'
+        ];
+        this.items = [];
+        for (var i = 1; i < 11; i++) {
+            this.items.push({
+                title: 'Item ' + i,
+                note: 'This is item #' + i,
+                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+            });
+        }
+    }
+    ListCopyPage.prototype.ngOnInit = function () {
+    };
+    ListCopyPage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-list',
+            template: __webpack_require__(/*! raw-loader!./listcopy.page.html */ "./node_modules/raw-loader/index.js!./src/app/listcopy/listcopy.page.html"),
+            styles: [__webpack_require__(/*! ./listcopy.page.scss */ "./src/app/listcopy/listcopy.page.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], ListCopyPage);
+    return ListCopyPage;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=listcopy-listcopy-module-es5.js.map
---
+++ www/listcopy2-listcopy2-module-es5.js
@@ -0,0 +1,130 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["listcopy2-listcopy2-module"],{
+
+/***/ "./node_modules/raw-loader/index.js!./src/app/listcopy2/listcopy2.page.html":
+/*!*************************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/listcopy2/listcopy2.page.html ***!
+  \*************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header>\r\n  <ion-toolbar>\r\n    <ion-title>\r\n      Arti\r\n    </ion-title>\r\n  </ion-toolbar>\r\n</ion-header>\r\n\r\n<ion-content>\r\n  <div margin>\r\n       Arti in English:\r\n\r\n      <p>\r\n          Jai Jagadish Hare\r\n          Swami Jaya Jagadish Hare\r\n          Bhakta janon ke sankat\r\n          Bhakta janon ke sankat Kshan me door kar\r\n          Om Jai Jagadish Hare\r\n      </p> \r\n\r\n      <p>Jo dhyave phal paave\r\n          Dhukh vinashe man ka\r\n          Swami dhukh vinashe man ka\r\n          Sukha sampati Ghar aave\r\n          Sukha sampati Ghar aave\r\n          Kashht mite tan ka\r\n          Om Jai Jagadish Hare\r\n        </p>\r\n    \r\n      <p>Pita tum mere\r\n          Sharan padun mai kis ki\r\n          Swami sharan padum mai kis ki\r\n          Tum bina aur na doojaa\r\n          Tum bina aur na doojaa\r\n          Asha karun mai kis ki\r\n          Om Jai Jagadish Hare\r\n      </p> \r\n\r\n      <p>Pooran Paramatma\r\n          Tum Antaryaami\r\n          Swami Tum Antaryaami\r\n          Para brahma Parameshwara\r\n          Para brahma Parameshwara\r\n          Tum sab ke Swami\r\n          Om Jai Jagadish Hare\r\n        </p> \r\n\r\n      <p>Karuna ke saagar\r\n          Tum palan karta\r\n          Swami Tum palan karta\r\n          Mai sevak tum swaami\r\n          Mai sevak tum swaami\r\n          Kripa karo bhartaa\r\n          Om Jai Jagadish Hare\r\n        </p> \r\n\r\n      <p>ho ek agochar\r\n          Sab ke prana pati\r\n          Swami sab ke prana pati\r\n          Kis vidhi miloon dayamaya\r\n          Kisi vidhi miloon dayamaya\r\n          Tum ko mai kumati\r\n          Om Jai Jagadish Hare\r\n      </p> \r\n        \r\n\r\n      <p>bandhu dukh hartaa\r\n          Tum rakshak mere\r\n          Swami tum rakshak mere\r\n          Apane hast uthao\r\n          Apane hast uthao\r\n          Dwar khada mai tere\r\n          Om Jai Jagadish Hare\r\n        </p> \r\n\r\n      <p>vikar mithao\r\n          Paap haro deva\r\n          Swami paap haro deva\r\n          Shraddha bhakti badhao\r\n          Shraddha bhakti badhao\r\n          Santan ki seva\r\n          Om Jai Jagadish Hare\r\n        </p> \r\n\r\n      <p>Tan man dhan sab kuch hai tera\r\n          Swami sab kuch hai tera\r\n          Tera tujh ko arpan\r\n          Tera tujh ko arpan\r\n          Kya laage mera\r\n          Om Jai Jagadish Hare\r\n        </p>\r\n\r\n      <p>Om Jai Jagadish Hare\r\n          Swami Jai Jagadish Hare\r\n          Bhakta janon ke sankat\r\n          Bhakta janon ke sankat\r\n          Kshan me door kare\r\n          Om Jai Jagadish Hare   \r\n        </p>   \r\n  </div>\r\n</ion-content>\r\n\r\n"
+
+/***/ }),
+
+/***/ "./src/app/listcopy2/listcopy2.module.ts":
+/*!***********************************************!*\
+  !*** ./src/app/listcopy2/listcopy2.module.ts ***!
+  \***********************************************/
+/*! exports provided: ListCopy2PageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ListCopy2PageModule", function() { return ListCopy2PageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _listcopy2_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./listcopy2.page */ "./src/app/listcopy2/listcopy2.page.ts");
+
+
+
+
+
+
+
+var ListCopy2PageModule = /** @class */ (function () {
+    function ListCopy2PageModule() {
+    }
+    ListCopy2PageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["IonicModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_5__["RouterModule"].forChild([
+                    {
+                        path: '',
+                        component: _listcopy2_page__WEBPACK_IMPORTED_MODULE_6__["ListCopy2Page"]
+                    }
+                ])
+            ],
+            declarations: [_listcopy2_page__WEBPACK_IMPORTED_MODULE_6__["ListCopy2Page"]]
+        })
+    ], ListCopy2PageModule);
+    return ListCopy2PageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/listcopy2/listcopy2.page.scss":
+/*!***********************************************!*\
+  !*** ./src/app/listcopy2/listcopy2.page.scss ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2xpc3Rjb3B5Mi9saXN0Y29weTIucGFnZS5zY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/listcopy2/listcopy2.page.ts":
+/*!*********************************************!*\
+  !*** ./src/app/listcopy2/listcopy2.page.ts ***!
+  \*********************************************/
+/*! exports provided: ListCopy2Page */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ListCopy2Page", function() { return ListCopy2Page; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var ListCopy2Page = /** @class */ (function () {
+    function ListCopy2Page() {
+        this.icons = [
+            'flask',
+            'wifi',
+            'beer',
+            'football',
+            'basketball',
+            'paper-plane',
+            'american-football',
+            'boat',
+            'bluetooth',
+            'build'
+        ];
+        this.items = [];
+        for (var i = 1; i < 11; i++) {
+            this.items.push({
+                title: 'Item ' + i,
+                note: 'This is item #' + i,
+                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+            });
+        }
+    }
+    ListCopy2Page.prototype.ngOnInit = function () {
+    };
+    ListCopy2Page = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-list',
+            template: __webpack_require__(/*! raw-loader!./listcopy2.page.html */ "./node_modules/raw-loader/index.js!./src/app/listcopy2/listcopy2.page.html"),
+            styles: [__webpack_require__(/*! ./listcopy2.page.scss */ "./src/app/listcopy2/listcopy2.page.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], ListCopy2Page);
+    return ListCopy2Page;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=listcopy2-listcopy2-module-es5.js.map
